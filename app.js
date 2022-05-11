86d6160506adbc152a403ec718e5e18f994c474b
gumodi35 <gumodi10@gmail.com>
2022-05-11 13:15:27 -0500

    creando endpoint y probandolo

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const express = require('express')
 // App de Express
 const app = express()
 // -> Indicamos que usaremos un JSON 
-app.use(express.json)
+app.use(express.json())
 // -> Puerto en que vamos a ver nuetra app: localhost:3000
 const port = 3000
 
+// -> GET crea un endpoint que regrese una lista de explorers
+// HTTP METHODS
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers GET ALL requests ${new Date()}`)
+    const explorer1 = {id: 1, name: "Gualberto1"}
+    const explorer2 = {id: 2, name: "Gualberto2"}
+    const explorer3 = {id: 3, name: "Gualberto3"}
+    const explorer4 = {id: 4, name: "Gualberto"}
+    const explorers = [explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
+
 // Con esto inicializamos esta app
-app.listenerCount(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
 
