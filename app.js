9fc9111088e8c31d91a0c1ba79ff0236ba33ad0c
gumodi35 <gumodi10@gmail.com>
2022-05-11 17:02:58 -0500

    agregando el metodo POST para crear explorer

--- app.js
+++ app.js
@@ -20,6 +20,22 @@ app.get('/v1/explorers', (req, res) => {
     res.status(200).json(explorers)
 })
 
+// -> Crea un endpoint que regresa un explorer mediando un ID
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1, name: "Gualberto"}
+    res.status(200).json(explorer)
+})
+
+// -> Crea un endpoint que se encargue de crear un explorer
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    // parametros de un cliente
+    const requestBody = req.body
+    res.status(201).json({message: "Created"})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
